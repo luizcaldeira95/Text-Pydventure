8a4e2c8c5cf86c36b03b55282fca4ab1f1fd5d51
Luiz Caldeira <109172099+luizcaldeira95@users.noreply.github.com>
2024-07-04 17:51:07 -0300

    Adicionando iniciais do projeto
    Arquivos adicionados:
    
    main - Arquivo principal no qual será contido as chamadas para funções alheias que dão funcionamento ao jogo, criado com o objetivo de simplificar a leitura do 
               funcionamento do sistema 
    
    glossario - Arquivo criado com o intuito de conter qualquer texto grande descritivo, o objetivo é separar as descrições do codigo principal pra uma melhor leitura do 
                     codigo
    
    criarpersonagem - Codigo responsável por todo o processo de criação de personagem

---
+++ glossario.py
@@ -0,0 +1,76 @@
+# Arquivo dedicado apenas a definições e descrições
+# O intuito é o de sempre usar esse arquivo como referencia para longos textos 
+# Sendo assim, a parte de codigo ficará mais limpa e facil de compriender
+
+
+# Cores ANSI para texto
+
+
+def descricaoRacas():
+
+    print("""
+            
+    Humano
+    Definição Geral: Humanos são uma raça versátil e adaptável, 
+    conhecida por sua diversidade cultural e habilidade de se adaptar a diferentes circunstâncias.
+
+    Classes Comuns: Humanos podem ser de qualquer classe disponível no jogo, 
+    como guerreiros, magos, clérigos, ladinos, etc.
+
+    Características Distintivas:
+
+    Bônus Racial: Recebem +1 em todos os atributos 
+    (Força, Destreza, Constituição, Inteligência, Sabedoria e Carisma).
+    Habilidade Extra: Ganham um ponto adicional em habilidades de sua escolha.
+    Armas e Habilidades: Podem utilizar uma ampla variedade de armas e 
+    habilidades dependendo da classe escolhida.
+
+    Elfo
+    Definição Geral: Elfos são uma raça grácil e mística, 
+    conhecida por sua longevidade, graciosidade e conexão com a natureza.
+
+    Sub-raças: Existem várias sub-raças de elfos, como 
+    Elfos da Floresta, Elfos Negros e Elfos Alto.
+
+    Classes Comuns: Elfos são frequentemente encontrados como arqueiros, 
+    magos e bardos, devido à sua agilidade e afinidade com a magia.
+
+    Características Distintivas (Exemplo para Elfos da Floresta):
+
+    Aumento de Atributo: +2 em Destreza e +1 em Sabedoria.
+    Sentidos Agudos: Tem vantagem em testes de resistência contra ser encantado e 
+    não pode ser colocado para dormir magicamente.
+    Transe Elfo: Pode meditar profundamente por 4 horas por dia, em vez de dormir,
+    e ainda ganha os benefícios de um descanso longo.
+    Armas e Habilidades: Geralmente são proficientes com 
+    arcos e espadas longas, e têm habilidades mágicas 
+    inatas dependendo da sub-raça.
+
+    Anões (Dwarves)
+    Definição Geral: Anões são uma raça robusta e resistente, 
+    conhecida por sua resistência, habilidades em trabalhar 
+    com metais e seu amor por festas e histórias.
+
+    Sub-raças: Incluem Anões da Montanha e Anões da Colina.
+
+    Classes Comuns: Anões são frequentemente guerreiros, 
+    clérigos e ferreiros, devido à sua força física e 
+    habilidades em trabalhar com metais.
+
+    Características Distintivas (Exemplo para Anões da Montanha):
+
+    Aumento de Atributo: +2 em Constituição.
+    Resistência Anã: Tem vantagem em testes de resistência contra venenos 
+    e tem resistência contra dano por veneno.
+    Treinamento de Combate Anão: São proficientes com machados de guerra, 
+    machados de batalha, martelos de guerra e martelos de guerra anões.
+    Armas e Habilidades: Preferem armas que podem ser forjadas, 
+    como machados e martelos, e têm habilidades relacionadas à 
+    resistência e durabilidade.
+
+    Características Gerais: Cada personagem possui seis 
+    características principais: Força, Destreza, Constituição, Inteligência,
+    Sabedoria e Carisma. Essas características determinam as habilidades e 
+    capacidades do personagem em várias situações.
+    """)
+
